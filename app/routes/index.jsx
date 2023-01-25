5d5a7561a8aee4f6b8e3f12cc73c7d57554807bf
Jared Rapp <jareddr@gmail.com>
2023-01-25 15:06:13 -0800

    Initial commit

---
+++ app/routes/index.jsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+<div className="relative container mx-auto text-center content-center max-w-2xl">
+  <img className='mx-auto md:max-w-2xl max-w-md' src='./images/vitruvian.gif' />
+    <p className='absolute text-5xl md:text-8xl lg:text-9xl top-1/2 left-[10%] md:left-[-4%] lg:left-[-24%] transform -translate-y-1/2 text-white uppercase bold' style={{'textShadow': '0 0 10px #3469c2, 0 0 20px #3469c2, 0 0 30px #71a3f6, 0 0 40px #71a3f6, 0 0 50px #5f92e9, 0 0 60px #5f92e9, 0 0 70px #5f92e9'}}>Humongous.ai</p>
+</div>
+  );
+}
